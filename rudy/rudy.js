dd8cb24c82e5a5a293cfd1189e74969d6e79543d
kabaliisa <kabaliisa15@gmail.com>
2021-11-02 23:06:46 -0500

    ix rudy

--- rudy/rudy.js
+++ rudy/rudy.js
@@ -1,19 +1,3 @@
-// var rudyTimer = (
-//     function() {
-//         timer = null;
-
-//         function rudy() {
-//             document.getElementById("output").innerHTML += " Rudy!";
-//         }
-//         var f = function delayMsg2() {
-//             if (timer === null) {
-//                 timer = setInterval(rudy, 1000)
-//             }
-//         }
-//         return f;
-//     }
-// )();
-
 var rudyTimer = (function() {
     timer = null; // stores ID of interval timer 
     function rudy() { // called each time the timer goes off
