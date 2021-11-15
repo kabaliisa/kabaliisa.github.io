335b9a3f88aa5e391271f76535af3dbc6ca713b6
kabaliisa <kabaliisa15@gmail.com>
2021-11-14 21:11:27 -0600

    change to let

--- NodeFolder/number-quiz/app.js
+++ NodeFolder/number-quiz/app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'pug')
-var questions = ["3,1,4,1,5", "1,1,2,3,5", "1,4,9,16,25", "2,3,5,7,11", "1,2,4,8,16"];
-var answers = [9, 8, 36, 13, 32];
-var index = 0;
+let questions = ["3,1,4,1,5", "1,1,2,3,5", "1,4,9,16,25", "2,3,5,7,11", "1,2,4,8,16"];
+let answers = [9, 8, 36, 13, 32];
+let index = 0;
 let score = 0;
 
 app.get('/', function(req, res) {
@@ -24,6 +24,6 @@ app.post('/result', function(req, res) {
     res.redirect('/');
 });
 
-var server = app.listen(3000, function() {
+let server = app.listen(3000, function() {
     console.log('Node server is running..');
 });
